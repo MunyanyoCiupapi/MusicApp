d8a83359ff94c6195fb58bb389b81c8238521cf3
Marcin Chmylko <chmylkomarcin@gmail.com>
2025-04-16 20:39:05 +0300

    connecting to mongodb

---
+++ public/js/db.js
@@ -0,0 +1,31 @@
+// db.js
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+const uri = `mongodb+srv://chmylkomarcin:${process.env.DB_PASS}@cluster.uvi44.mongodb.net/?retryWrites=true&w=majority&appName=Cluster`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+let db;
+
+async function connectToMongo() {
+  try {
+    await client.connect();
+    db = client.db("musicstore"); 
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+  }
+}
+
+function getDb() {
+  return db;
+}
+
+module.exports = { connectToMongo, getDb };
--- public/js/server.js
+++ public/js/server.js
@@ -1,23 +1,44 @@
 const express = require('express');
+const { connectToMongo, getDb } = require('./db'); 
+require('dotenv').config();
+
 const app = express();
-
 const port = process.env.PORT || 3000;
-
 
 app.use(express.json());
 app.use(express.static('public'));
 
 
-app.get('/message', (req, res) => {
+connectToMongo().then(() => {
+  const db = getDb();
+
+
+  
+  db.collection('users').insertOne({ name: 'John Doe', age: 30 })
+    .then(() => {
+      console.log('Inserted data into users collection');
+    })
+
+  app.get('/message', (req, res) => {
     res.json({ message: 'Hello from the Marcin!' });
-})
+  });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+
+  
+  app.get('/users', async (req, res) => {
+    try {
+      const users = await db.collection('users').find().toArray();
+      res.json(users);
+    } catch (err) {
+      res.status(500).send("Error fetching users" + err);
+    }
+  });
+
+  app.get('/', (req, res) => {
+    res.send('Hello, World!');
+  });
+
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
 });
-
-
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
-
